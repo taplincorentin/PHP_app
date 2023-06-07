db85f8cae10763950635d938438c88f02ae3a73f
Corentin Taplin <taplincorentin@gmail.com>
2023-06-07 11:39:58 +0200

    added dropdown menu button

--- appli/index.php
+++ appli/index.php
@@ -4,12 +4,21 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
         <title>Product adding</title>
     </head>
     <body>
-
-        <h1>Add a new product</h1>
-        <form action="processing.php" method="post"> <!--form action gives target file when form submitted
+        <nav>
+            <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown"> 
+                MENU 
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="index.php">Add new product</a></li>
+                <li><a class="dropdown-item" href="recap.php">Recap</a></li>
+            </ul>
+        </nav>
+        <h1 class ="text-center">Add a new product</h1>
+        <form action="processing.php" method="post" class ="text-center"> <!--form action gives target file when form submitted
                                                             method gives HTTP method that will be used to transmit form to server -->
             <p>
                 <label>
@@ -30,7 +39,7 @@
                 </label>
             </p>
             <p>
-                <input type='submit' name='submit' value="Add the product">
+                <input type='submit' name='submit' value="Add the product" class="btn btn-primary">
             </p>
         </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
--- appli/recap.php
+++ appli/recap.php
@@ -7,10 +7,20 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        
         <title>Products recap</title>
 </head>
 <body>
+    <nav>
+        <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown"> 
+            MENU 
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="index.php">Add new product</a></li>
+            <li><a class="dropdown-item" href="recap.php">Recap</a></li>
+        </ul>
+    </nav>
     <?php 
         //si la clé 'products' n'existe pas ou est nulle affichage d'un message
         if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
@@ -48,5 +58,6 @@
                 "</table>";
         }
     ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
